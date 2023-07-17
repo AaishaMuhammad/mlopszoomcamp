1f95a3f7666723b9d101686b616679942f701ed2
AaishaM <aaisha@pearlcoral.com>
2023-07-17 21:23:18 +0200

    wk 6 hw

---
+++ week-06/integration_test.py
@@ -0,0 +1,43 @@
+import os
+import pandas as pd
+
+from datetime import datetime
+
+year = 2022
+month = int('02')
+
+options = {
+    'client_kwargs': {
+        'endpoint_url': os.getenv('S3_ENDPOINT_URL')
+    }
+}
+
+def get_input_path(year, month):
+    default_input_pattern = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    input_pattern = os.getenv('INPUT_FILE_PATTERN', default_input_pattern)
+    return input_pattern.format(year=year, month=month)
+
+def dt(hour, minute, second=0):
+    return datetime(2022, 1, 1, hour, minute, second)
+
+data = [
+        (None, None, dt(1, 2), dt(1, 10)),
+        (1, None, dt(1, 2), dt(1, 10)),
+        (1, 2, dt(2, 2), dt(2, 3)),
+        (None, 1, dt(1, 2, 0), dt(1, 2, 50)),
+        (2, 3, dt(1, 2, 0), dt(1, 2, 59)),
+        (3, 4, dt(1, 2, 0), dt(2, 2, 1)),     
+    ]
+
+columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+df_input = pd.DataFrame(data, columns=columns)
+input_file=get_input_path(year, month)
+
+
+df_input.to_parquet(
+    input_file,
+    engine='pyarrow',
+    compression=None,
+    index=False,
+    storage_options=options
+)
